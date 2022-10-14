74590dbe05660f8af5d9d30e4ce91b2fb3795e46
Hemanth Saju <107930234+hemanthsaju7@users.noreply.github.com>
2022-10-15 02:16:58 +0530

    Create variables.tf

---
+++ vpc-module/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "example"
+}
+
+variable "project_env" {
+  default = "testing"
+}
+
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
